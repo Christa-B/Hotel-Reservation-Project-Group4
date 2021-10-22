391c3b05521eecf8bf632ce4ecdc70d0d8c0f155
Christa-B <91355595+Christa-B@users.noreply.github.com>
2021-10-22 05:31:40 -0500

    Update ResultsController.java

--- Hotel_Reservation_Project_Group4/src/controller/ResultsController.java
+++ Hotel_Reservation_Project_Group4/src/controller/ResultsController.java
@@ -1,4 +1,4 @@
-/***
+/**
  * Hotel Reservation Project
  * UTSA CS 3773-002 - Fall 2021
  * Team 4: Andres De La Rosa, Jackson Raymond, Jalyn Merritt, Aden Rojas, Christa Baca 
@@ -7,94 +7,96 @@
 package controller;
 
 import application.Main;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.util.Callback;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.image.*;
+import javafx.stage.Stage;
+import javafx.scene.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.*;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.DateCell;
+import javafx.scene.input.*;
 
 /**
- * SignUpController is a class that handles events that occur when the user
- * interacts with signup_screen.fxml
+ * ResultsController is a class that handles events that occur when the user
+ * interacts with results_page.fxml
  * 
- * @author Christa
+ * @author Christa Baca
  */
-public class SignUpController implements Initializable {
+public class ResultsController implements Initializable{
 	// Buttons
 	@FXML
-	private Button button; // JOIN button
+	private Button button; // Search Button
 	
 	// Labels
 	@FXML
-	private Label label1; // Project Name
+	private Label label1; // Results Page
 	
 	@FXML
-	private Label label2; // Create Account
-	
-	@FXML
-	private Label label3; // first name
-	
-	@FXML
-	private Label label4; // last name
-	
-	@FXML
-	private Label label5; // email
-	
-	@FXML
-	private Label label6; // phone number
-	
-	@FXML
-	private Label label7; // password
-	
-	@FXML
-	private Label label8; // account type
-	
-	@FXML
-	private Label label9; // Already have an account?
+	private Label label2; // Placeholder for results info
 	
 	// HyperLinks
 	@FXML
-	private Hyperlink hyperlink; // Login here.
+	private Hyperlink hyperlink1; // PROJECT NAME
 	
 	@FXML
-	private Hyperlink homehandler;
+	private Hyperlink hyperlink2; // My Reservation
 	
-	// ChoiceBoxes
 	@FXML
-	private ComboBox combobox; // Option for customer/administrator account type
+	private Hyperlink hyperlink3; // Account Settings
 	
+	@FXML
+	private Hyperlink hyperlink4; // Logout
+	
+	// MenuButtons
+	@FXML
+	private MenuButton menubutton; // Amenities
+	
+	// ComboBoxes
+	@FXML
+	private ComboBox<String> combobox1; // Number of Rooms
+	
+	@FXML
+	private ComboBox<String> combobox2; // Price Ranges
+	
+	// DatePickers 
+	@FXML
+	private DatePicker datepicker; // Check-in/Check-out dates
+	
+	// List of items for ComboBoxes (prices and rooms)
+	ObservableList<String> list1 = FXCollections.observableArrayList("2 Rooms", "3 Rooms", "4 Rooms", "5 Rooms", "6 Rooms", "7 Rooms");
+	ObservableList<String> list2 = FXCollections.observableArrayList("Low", "Mid", "High");
+
 	// Static variables to set style for button when mouse is away/hovering
 	private static String normal_button_style = "-fx-background-color: white; -fx-background-radius: 20;";
 	private static String hovered_button_style = "-fx-background-color: #d3d3d3; -fx-background-radius: 20;";
 	
-	// List of items for ComboBoxes (prices and rooms)
-	ObservableList<String> list1 = FXCollections.observableArrayList("Customer", "Admin");
-
 	/**
-	 * This method will set a different style for button depending on whether or not mouse hovers it
+	 * Initializes items for Number of Rooms and Price Range ComboBoxes
 	 * 
 	 * @param location  location properties for controller and FXMLLoader
 	 * @param resources  resources for controller and FXMLLoader
 	 */
 	@Override
 	public void initialize( URL location, ResourceBundle resources ) {
-		// Sets choicebox items
-		combobox.setItems(list1);
+		// Sets list items for ComboBoxes
+		combobox1.setItems(list1);
+		combobox2.setItems(list2);
 		
 		// Normal button style set to white
 	    button.setStyle(normal_button_style);
@@ -107,12 +109,25 @@ public class SignUpController implements Initializable {
 	}
 	
 	/**
+	 * Handles event in which user wants to check their reservation
+	 * 
+	 * @param event  event in which user clicks My Reservation HyperLink
+	 * @throws IOException  if a file is unable to be read
+	 */
+	@FXML
+	public void handleMyReservation(ActionEvent event) throws IOException {
+		// Loads the FXML document for account_settings and displays it
+		Parent root = FXMLLoader.load(getClass().getResource("/application/user_reservation.fxml"));
+		Stage window = (Stage)button.getScene().getWindow();
+		window.setScene(new Scene (root));
+	}
+	
+	/**
 	 * Changes view to the HOME PAGE after button is clicked
 	 * 
 	 * @param event	 event in which user clicks on the Project Name button
 	 * @throws IOException	if a file is unable to be read
 	 */
-	
 	@FXML
 	public void handleBackToHomePage( ActionEvent event ) throws IOException {
 		// Loads the FXML document for home_page and displays it
@@ -122,27 +137,27 @@ public class SignUpController implements Initializable {
 	}
 	
 	/**
-	 * Changes view to the HOME PAGE after button is clicked
+	 * Handles event in which user wants to change account settings
 	 * 
-	 * @param event	 event in which user clicks on the LOGIN button
-	 * @throws IOException	if a file is unable to be read
+	 * @param event  event in which user clicks Account Settings HyperLink
+	 * @throws IOException  if a file is unable to be read
 	 */
 	@FXML
-	public void handleSignIn( ActionEvent event ) throws IOException {
-		// Loads the FXML document for home_page and displays it
-		Parent root = FXMLLoader.load(getClass().getResource("/application/home_page.fxml"));
+	public void handleAccountSettings(ActionEvent event) throws IOException {
+		// Loads the FXML document for account_settings and displays it
+		Parent root = FXMLLoader.load(getClass().getResource("/application/account_settings.fxml"));
 		Stage window = (Stage)button.getScene().getWindow();
 		window.setScene(new Scene (root));
 	}
 	
 	/**
-	 * Changes view to the LOGIN PAGE after HyperLink is clicked
+	 * Handles event in which user wants to logout from account
 	 * 
-	 * @param event	 event in which user clicks on the Login here HyperLink
-	 * @throws IOException	if a file is unable to be read
+	 * @param event  event in which user clicks Logout HyperLink
+	 * @throws IOException  if a file is unable to be read
 	 */
 	@FXML
-	public void handleLoginPage( ActionEvent event ) throws IOException {
+	public void handleLogout(ActionEvent event) throws IOException {
 		// Loads the FXML document for login_screen and displays it
 		Parent root = FXMLLoader.load(getClass().getResource("/application/login_screen.fxml"));
 		Stage window = (Stage)button.getScene().getWindow();
