48c793c1236c6b75ee7bdea44e452b75a8b5e2b3
Christa-B <91355595+Christa-B@users.noreply.github.com>
2021-11-11 14:34:09 -0600

    Add files via upload

--- Hotel_Reservation_Project_Group4/src/application/Main.java
+++ Hotel_Reservation_Project_Group4/src/application/Main.java
@@ -30,7 +30,7 @@ public class Main extends Application {
 		// Try and catch block for exception handling
 		try {	
 			// Load the FXML document
-			Parent root = FXMLLoader.load( getClass().getResource( "/application/home_page3.fxml"));
+			Parent root = FXMLLoader.load( getClass().getResource( "/application/home_page.fxml"));
 			
 			// Set the scene to stage and show the stage to the user
 			//Scene scene = new Scene( root, 1294, 694 );
